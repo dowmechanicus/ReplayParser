7d93a921006cf0422b8264fb7dca9aca01b77053
Adeptus Noobus <adeptus.noobus@gmail.com>
2022-01-16 14:20:41 +0100

    Add warlock wargear IDs

--- src/actions/purchase_wargear.rs
+++ src/actions/purchase_wargear.rs
@@ -3,6 +3,7 @@ use super::*;
 #[derive(Debug, Serialize)]
 pub struct PurchaseWargearAction {
     pub unit_id: u8,
+    pub wargear_id: u8,
     pub wargear_name: String,
 }
 impl ParseAction for PurchaseWargearAction {}
@@ -12,11 +13,13 @@ impl<'a> From<ActionData<'a>> for PurchaseWargearAction {
         if data.len() > 13 {
             Self {
               unit_id: data[10],
+              wargear_id: data[13],
               wargear_name: get_wargear_by_item_id(data[13]).to_string(),
             }
         } else {
             Self {
               unit_id: 0,
+              wargear_id: data[13],
               wargear_name: "".to_string(),
             }
         }
@@ -25,17 +28,27 @@ impl<'a> From<ActionData<'a>> for PurchaseWargearAction {
 
 fn get_wargear_by_item_id(id: u8) -> &'static str {
   match id {
-      197 => "Assault Marine Sergeant",
-      216 => "Scout - Elite training",
-      217 => "Scout - Sergeant",
-      218 => "Scout - Shotguns",
-      227 => "Vanguard Veteran Squad",
-      231 => "Force Commander - Iron Halo",
-      235 => "Techmarine - Refractor Shield",
-      240 => "Force Commander - Alacrity armor",
-      241 => "Terminator Force Commander",
-      242 => "Techmarine - Artificer Armor",
-      252 => "Force Commander - Power Sword",
-      _ => "Unknown wargear",
+    133 => "Immolator",
+    198 => "Champions Robe",
+    109 => "Channeling Runes",
+    186 => "Merciless Witchblade",
+    176 => "Cloak of Shadows",
+    197 => "Warp Throw",
+    187 => "Witchblade of Kurnous",
+    178 => "Providence",
+    144 => "Heart of Darkness",
+    196 => "Faolchu's Wing",
+    197 => "Assault Marine Sergeant",
+    216 => "Scout - Elite training",
+    217 => "Scout - Sergeant",
+    218 => "Scout - Shotguns",
+    227 => "Vanguard Veteran Squad",
+    231 => "Force Commander - Iron Halo",
+    235 => "Techmarine - Refractor Shield",
+    240 => "Force Commander - Alacrity armor",
+    241 => "Terminator Force Commander",
+    242 => "Techmarine - Artificer Armor",
+    252 => "Force Commander - Power Sword",
+    _ => "Unknown wargear",
   }
 }
